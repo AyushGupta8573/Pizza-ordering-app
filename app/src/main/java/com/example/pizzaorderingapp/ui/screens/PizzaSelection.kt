3a3045ec17327e19de2722aa2ebc029f0e8962f4
ayushgupta <ayushgupta160803@gmail.com>
2024-07-24 03:34:50 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pizzaorderingapp/ui/screens/PizzaSelection.kt
@@ -0,0 +1,18 @@
+package com.example.pizzaorderingapp.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+
+@Composable
+fun PizzaSelectionScreen(modifier: Modifier = Modifier) {
+    Box (modifier = modifier
+        .fillMaxSize()
+        .background(Color.Red)){
+
+    }
+}
